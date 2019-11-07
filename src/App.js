e6daebb79c0331978557f969f0984047cdda9ea9
dhchan <daniel.hoyin.chan@gmail.com>
2019-11-06 17:15:47 -0800

    working commit, separated Feature from featureform

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ class App extends Component {
   };
 
   updateFeature = (feature, newValue) => {
+    console.log(newValue)
     const selected = Object.assign({}, this.state.selected);
     selected[feature] = newValue;
     this.setState({
--- src/FeaturesForm.js
+++ src/FeaturesForm.js
@@ -1,51 +1,17 @@
 import React, { Component } from 'react';
-import slugify from 'slugify';
+import Feature from './Feature';
 
-
-// This object will allow us to
-// easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
 
 class FeaturesForm extends Component {
-  /*render() {
-    console.log(this.props.features)
-    return (
-      <h1>test</h1>
-    )
-  }*/
   render() {
-    console.log(this.props.features)
     const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.props.selected[feature].name}
-              onChange={e => this.props.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-              </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
+      return(
+        <Feature 
+          feature={feature}
+          idx={idx}
+          options={this.props.features[feature]}
+          selected={this.props.selected}
+          updateFeature={this.props.updateFeature} />
       );
     });
 
