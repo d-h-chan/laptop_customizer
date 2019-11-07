14f824b6b5d6e10600b6eebc86a069d8fce94cf9
dhchan <daniel.hoyin.chan@gmail.com>
2019-11-06 16:30:41 -0800

    moved form and header out from app.js, working commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
+import Header from './Header'
+import FeaturesForm from './FeaturesForm'
 
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from 'slugify';
 
 import './App.css';
 
@@ -44,36 +43,6 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
 
     const summary = Object.keys(this.state.selected).map((feature, idx) => {
       const featureHash = feature + '-' + idx;
@@ -98,13 +67,13 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <h1>ELF Computing | Laptops</h1>
+          <Header/>
         </header>
         <main>
-          <form className="main__form">
-            <h2>Customize your laptop</h2>
-            {features}
-          </form>
+          <FeaturesForm 
+            features={this.props.features} 
+            selected={this.state.selected}
+            updateFeature={this.updateFeature}/>
           <section className="main__summary">
             <h2>Your cart</h2>
             {summary}
---
+++ src/FeaturesForm.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import slugify from 'slugify';
+
+
+// This object will allow us to
+// easily convert numbers into US dollar values
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
+
+class FeaturesForm extends Component {
+  /*render() {
+    console.log(this.props.features)
+    return (
+      <h1>test</h1>
+    )
+  }*/
+  render() {
+    console.log(this.props.features)
+    const features = Object.keys(this.props.features).map((feature, idx) => {
+      const featureHash = feature + '-' + idx;
+      const options = this.props.features[feature].map(item => {
+        const itemHash = slugify(JSON.stringify(item));
+        return (
+          <div key={itemHash} className="feature__item">
+            <input
+              type="radio"
+              id={itemHash}
+              className="feature__option"
+              name={slugify(feature)}
+              checked={item.name === this.props.selected[feature].name}
+              onChange={e => this.props.updateFeature(feature, item)}
+            />
+            <label htmlFor={itemHash} className="feature__label">
+              {item.name} ({USCurrencyFormat.format(item.cost)})
+              </label>
+          </div>
+        );
+      });
+
+      return (
+        <fieldset className="feature" key={featureHash}>
+          <legend className="feature__name">
+            <h3>{feature}</h3>
+          </legend>
+          {options}
+        </fieldset>
+      );
+    });
+
+    return (
+      <form className="main__form">
+        <h2>Customize your laptop</h2>
+        {features}
+      </form>
+    )
+  }
+}
+
+export default FeaturesForm;
+
+
